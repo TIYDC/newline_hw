c69a672886d1002c992de5bcd1665953a522d2ad
Russell Osborne <russell@burningpony.com>
2016-08-09 23:12:21 -0400

    Revert to only load atom

--- lib/tiyo_hw.rb
+++ lib/tiyo_hw.rb
@@ -6,7 +6,8 @@ require "active_support/core_ext/string"
 module TiyoHw
   SLEEP_TIME = 4
   HOMEWORK_DIR = "~/theironyard/homework".freeze
-  EDITOR = ENV['EDITOR'] || "atom".freeze
+  # EDITOR = ENV['EDITOR'] || "atom".freeze
+  EDITOR = "atom".freeze
 
   def self.generate_cmd(command, url_or_path = nil)
     return ShellFunction.cmd if command == "init"
