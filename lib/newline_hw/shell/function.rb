5ebc71e166abc08707d93906c0d8c149e05b69d0
Russell Osborne <russell@burningpony.com>
2017-01-07 17:42:15 -0500

    Run command must be built after the setup command has completed.

--- lib/newline_hw/shell/function.rb
+++ lib/newline_hw/shell/function.rb
@@ -27,12 +27,13 @@ function #{HW_FUNCTION}() {
   setup_command=$(#{path} setup_command $*);
   setup_es=$?;
 
-  run_command=$(#{path} run_command $PWD $*);
-  run_es=$?;
-
   if [ $setup_es = 0 ]; then;
     eval $setup_command;
     setup_command_es=$?;
+
+    run_command=$(#{path} run_command $PWD $*);
+    run_es=$?;
+
     if [ $run_es = 0 ] && [ $setup_command_es = 0 ]; then;
       eval $run_command;
     else;
