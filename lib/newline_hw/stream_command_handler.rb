af7adcf9bc72b835c649e9119790d477e062d2a7
Russell Osborne <russell@burningpony.com>
2017-01-06 20:55:52 -0500

    Use message_at vs, message_id.

--- lib/newline_hw/stream_command_handler.rb
+++ lib/newline_hw/stream_command_handler.rb
@@ -2,11 +2,11 @@ require "json"
 
 module NewlineHw
   class StreamCommandHandler
-    attr_reader :event, :data, :message_id
+    attr_reader :event, :data, :message_at
     def initialize(event)
       @event = event["event"].to_sym
       @data = event["data"]
-      @message_id = event["message_id"].to_i
+      @message_at = event["message_at"].to_i
     end
 
     def call
@@ -15,20 +15,20 @@ module NewlineHw
         when :heartbeat
           {
             status: :ok,
-            message_id: message_id,
+            message_at: message_at,
             data: {
               version: NewlineHw::VERSION,
               ruby_version: RUBY_VERSION
             }
           }
         when :clone_and_open_submission
-          { status: :ok, message_id: message_id, data: GuiTrigger.new(data).call }
+          { status: :ok, message_at: message_at, data: GuiTrigger.new(data).call }
         else
-          { event: @event, message_id: message_id, status: :fail, message: "no event handler found in Stream Command Handler" }
+          { event: @event, message_at: message_at, status: :fail, message: "no event handler found in Stream Command Handler" }
         end
 
       rescue StandardError => e
-        return { event: @event, message_id: message_id, status: :fail, data: e.message }
+        return { event: @event, message_at: message_at, status: :fail, data: e.message }
       end
     end
   end
